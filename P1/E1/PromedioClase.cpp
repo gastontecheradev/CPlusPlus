48b0d891151e6d6383739090f770df59833f2bee
gastontecheradev <acorde.musica10@gmail.com>
2026-03-09 12:41:39 -0300

    abc

--- P1/E1/PromedioClase.cpp
+++ P1/E1/PromedioClase.cpp
@@ -1,9 +1,47 @@
 #include <stdio.h>
 #include <conio.h>
 
+const int N = 5;
+
+// Promedio de las 4 calificaciones mayores entre 5
+float PromClase(int calArr[])
+{
+    int menor = calArr[0];
+    int suma = 0;
+
+    // Buscar la menor calificacion y sumar las calificaciones
+    for(int i=0; i<N; i++)
+    {
+        if(calArr[i] < menor)
+        {
+            menor = calArr[i];
+        }
+
+        suma += calArr[i];
+    }
+
+    suma -= menor;
+
+    return (float)suma / (N-1);
+
+}
+
 int main()
 {
-    printf("Promedio de la Clase");
+    float resPromedio;
+    int notasArr[N];
+
+    // Solicitar calificaciones
+    for(int i=0; i<N; i++)
+    {
+        printf("Ingrese la calificacion numero %d: ", i+1);
+        scanf("%d", &notasArr[i]);
+    }
+
+    resPromedio = PromClase(notasArr);
+
+    printf("El promedio es: %.2f\n", resPromedio);
+
     getch();
     return 0;
 }
