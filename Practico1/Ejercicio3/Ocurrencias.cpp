39fd5c19f4e2f977411e1b16ef498df6bcf2cc68
gastontecheradev <acorde.musica10@gmail.com>
2026-03-12 15:34:26 -0300

    abc

---
+++ Practico1/Ejercicio3/Ocurrencias.cpp
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <conio.h>
+
+// Crear la funcion Ocurrencias con los parametros frase, largo y caracter
+int Ocurrencias(char frase[], int largo, char caracter)
+{
+    int suma = 0;
+
+    // Contar cuantas veces aparece la letra en la frase
+    for(int i = 0; i < largo; i++)
+    {
+        if (frase[i] == caracter)
+        {
+            suma += 1;
+        }
+    }
+
+    return suma;
+}
+
+int main()
+{
+    char frase[100];
+    char letra;
+    int i;
+    int largo = 0;
+
+    //  Solicitar la frase y la letra al usuario
+    printf("CANTIDAD DE OCURRENCIAS\n\n");
+
+    printf("Ingrese la letra: ");
+    scanf(" %c", &letra);
+
+    printf("Ingrese la frase: ");
+
+    scanf("%c", frase[0]);
+    
+    // Crear un array de caracteres con la frase ingresada
+    // Crear la variable largo
+    // Crear la variable letra
+    for (i = 0; i < 99; i++)
+    {
+        frase[i] = getchar();
+     
+        if (frase[i] == '\n')
+        {
+            break;
+        }
+
+        largo++;
+    }
+
+    frase[i] = '\0';
+
+    printf("Frase %s\n", frase);
+
+    printf("Largo: %d\n", largo);
+
+    int cantOcurrencias = Ocurrencias(frase, largo, letra);
+
+    printf("Cantidad de ocurrencias: %d", cantOcurrencias);
+
+    getch();
+    return 0;
+}
---
+++ Practico1/Ejercicio3/README.txt
@@ -0,0 +1,11 @@
+Ejercicio 3 Ocurrencias
+
+Se quiere implementar una función que cuente la cantidad de veces que una letra aparece en una frase.
+
+La frase se representa como un arreglo de caracteres, y dado que se conoce que el largo máximo de
+una frase es de 100 caracteres, la frase se implementa como char frase[100].
+
+Usando esta representación, escriba una función Ocurrencias que recibe una frase,
+un natural llamado largo que representa el número
+de caracteres en la frase, y el caracter a buscar (almacenado en la variable letra), y devuelve el número de
+ocurrencias del carácter letra en el arreglo frase.
